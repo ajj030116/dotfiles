53cb2fbfc6b09b3831437c74556834829a040f9c
ajj030116 <ajj030116@gmail.com>
2016-10-10 15:45:40 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt-get install -y zsh htop tmux python-dev python-pip ruby build-essential
+sudo gem install --no-rdoc --no-ri tmuxinator
+sudo pip install glances
+git clone --recursive https://github.com/ajj030116/prezto.git ~/.zprezto
+git clone https://github.com/ajj030116/tpm.git ~/.tmux/plugins/tpm
+git clone https://github.com/ajj030116/fonts.git ~/.powerline-fonts
+git clone https://github.com/ajj030116/tmux-powerline.git ~/.tmux-powerline
+~/.powerline-fonts/install.sh
