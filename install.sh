bcd2642cff55cbb949215308db6f374dfe4f485b
ajj030116 <ajj030116@gmail.com>
2016-10-10 16:12:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ git clone https://github.com/ajj030116/tpm.git ~/.tmux/plugins/tpm
 git clone https://github.com/ajj030116/fonts.git ~/.powerline-fonts
 git clone https://github.com/ajj030116/tmux-powerline.git ~/.tmux-powerline
 ~/.powerline-fonts/install.sh
+
+# now run `sudo vipw` and edit the /etc/passwd file for user to use /usr/bin/zsh as shell
+# next time you open tmux run `prefix + I` to install plugins
